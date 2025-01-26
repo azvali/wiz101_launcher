6329016511755a9574782f96ae3357d7e29c2999
Yousef <104654454+azvali@users.noreply.github.com>
2025-01-26 15:46:30 -0500

    Add files via upload

---
+++ launcher.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\ProgramData\KingsIsle Entertainment\Wizard101\Bin"
+start "" WizardGraphicalClient.exe -L login.us.wizard101.com 12000 
---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+import pyautogui
+import time
+
+
+
+bat_file_path = r"launcher.bat"
+
+#add account logins here in a key : value pair format
+hashmap ={}
+
+for username, password in hashmap.items():
+    
+    process = subprocess.Popen([bat_file_path], shell=True)
+    print(f"launching client for {username}")
+    time.sleep(4)
+    
+    pyautogui.typewrite(username)
+
+    pyautogui.press("tab")
+
+    pyautogui.typewrite(password)
+
+    pyautogui.press("enter")  
+    time.sleep(2)
+
+print("clients launched")
